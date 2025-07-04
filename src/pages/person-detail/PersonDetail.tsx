cb9cfd6a0f15bd2bfe92a7d79714a11e61d8bfa1
Muhammadali <muhamadalimamadaminov@gmail.com>
2025-07-04 09:33:19 +0300

    person

--- src/pages/person-detail/PersonDetail.tsx
+++ src/pages/person-detail/PersonDetail.tsx
@@ -57,19 +57,4 @@ const PersonDetail = () => {
 
 export default React.memo(PersonDetail);
 
-{/* {combinedCredits?.cast?.length > 0 && (
-<div className="mb-12">
-    <h2 className="text-2xl font-semibold mb-4 border-l-4 border-red-500 pl-3">Filmlari</h2>
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-    {combinedCredits.cast.filter((movie: ICastMovie) => movie.poster_path).slice(0, 10).map((movie: ICastMovie) => (
-        <div key={movie.id} className="bg-white dark:bg-[#1a1a1a] rounded-xl overflow-hidden" onClick={() => navigate(`/movie/${movie.id}`)}>
-            <img src={ IMAGE_URL + movie.poster_path} alt={movie.title} className="w-full h-60 object-cover"/>
-            <div className="p-3">
-            <h3 className="text-sm font-semibold truncate" title={movie.title}>{movie.title}</h3>
-            <p className="text-xs text-gray-500 dark:text-gray-400">{movie.release_date?.split("-")[0] || "Yil yo'q"}</p>
-            </div>
-        </div>
-        ))}
-    </div>
-</div>
-)} */}
+
